9649e64aca83eecf9e409a1a09c02662db56de40
LEales <102584475+LEales@users.noreply.github.com>
2023-02-12 14:03:47 +0100

    unique expected results

--- src/test/java/view/Menu/PlayerScoreTest.java
+++ src/test/java/view/Menu/PlayerScoreTest.java
@@ -9,7 +9,9 @@ class PlayerScoreTest {
     @Test
     void createValid() {
         PlayerScore ps = new PlayerScore("ABC", 1, 2, 3, 11);
-        assertEquals(3723, ps.getTotalTimeInSeconds());
+        assertAll(() -> assertEquals("ABC", ps.getPlayer()),
+                () -> assertEquals(11, ps.getLvl()),
+                () -> assertEquals(3723, ps.getTotalTimeInSeconds()));
     }
 
     @Test
@@ -17,7 +19,8 @@ class PlayerScoreTest {
         try {
             new PlayerScore("ABCC", 1, 2, 3, 11);
             fail("Four chars not allowed");
-        } catch (IllegalArgumentException ignored) {
+        } catch (IllegalArgumentException e) {
+            assertEquals("Name length invalid", e.getMessage());
         }
     }
 
@@ -26,7 +29,8 @@ class PlayerScoreTest {
         try {
             new PlayerScore("AV", 1, 2, 3, 11);
             fail("Two chars not allowed");
-        } catch (IllegalArgumentException ignored) {
+        } catch (IllegalArgumentException e) {
+            assertEquals("Name length invalid", e.getMessage());
         }
     }
 
@@ -35,7 +39,8 @@ class PlayerScoreTest {
         try {
             new PlayerScore("\n", 0, 0, 0, 11);
             fail("NewLine not allowed");
-        } catch (IllegalArgumentException ignored) {
+        } catch (IllegalArgumentException e) {
+            assertEquals("Name not allowed to contain new line", e.getMessage());
         }
     }
 
@@ -44,8 +49,8 @@ class PlayerScoreTest {
         try {
             new PlayerScore("\t", 0, 0, 0, 11);
             fail("Tab not allowed");
-        } catch (IllegalArgumentException ignored) {
-
+        } catch (IllegalArgumentException e) {
+            assertEquals("Name not allowed to contain tab", e.getMessage());
         }
     }
 
@@ -54,8 +59,8 @@ class PlayerScoreTest {
         try {
             new PlayerScore("", 0, 0, 0, 11);
             fail("Empty string not allowed");
-        } catch (IllegalArgumentException ignored) {
-
+        } catch (IllegalArgumentException e) {
+            assertEquals("Name length invalid", e.getMessage());
         }
     }
 
@@ -64,38 +69,58 @@ class PlayerScoreTest {
         try {
             new PlayerScore(null, 0, 0, 0, 11);
             fail("Null not allowed");
-        } catch (IllegalArgumentException ignored) {
-
+        } catch (IllegalArgumentException e) {
+            assertEquals("Name not allowed to be null", e.getMessage());
         }
     }
 
     @Test
-    void invalidSeconds() {
+    void invalidLowerSeconds() {
         try {
             new PlayerScore("ABC", 0, 0, -1, 11);
             fail("Negative seconds not allowed");
-        } catch (IllegalArgumentException ignored) {
-
+        } catch (IllegalArgumentException e) {
+            assertEquals("Invalid seconds", e.getMessage());
         }
     }
 
     @Test
-    void invalidMinutes() {
+    void invalidLowerMinutes() {
         try {
             new PlayerScore("ABC", 0, -1, 0, 11);
             fail("Negative minutes not allowed");
-        } catch (IllegalArgumentException ignored) {
-
+        } catch (IllegalArgumentException e) {
+            assertEquals("Invalid minutes", e.getMessage());
         }
     }
 
     @Test
-    void invalidHours() {
+    void invalidLowerHours() {
         try {
             new PlayerScore("ABC", -1, 0, 0, 11);
             fail("Negative hours not allowed");
-        } catch (IllegalArgumentException ignored) {
+        } catch (IllegalArgumentException e) {
+            assertEquals("Invalid hours", e.getMessage());
+        }
+    }
 
+    @Test
+    void invalidUpperSeconds() {
+        try {
+            new PlayerScore("ABC", 0, 0, 60, 11);
+            fail("Seconds over 59 not allowed");
+        } catch (IllegalArgumentException e) {
+            assertEquals("Invalid seconds", e.getMessage());
+        }
+    }
+
+    @Test
+    void invalidUpperMinutes() {
+        try {
+            new PlayerScore("ABC", 0, 60, 0, 11);
+            fail("Minutes over 59 not allowed");
+        } catch (IllegalArgumentException e) {
+            assertEquals("Invalid minutes", e.getMessage());
         }
     }
 }
