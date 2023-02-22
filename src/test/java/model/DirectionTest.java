c36556800aa912286252bc702a8ce7250a2d7d5d
azamsuleiman <azam.suleiman@gmail.com>
2023-02-22 08:06:02 +0100

    Lagt till tester för fler klasser i model

---
+++ src/test/java/model/DirectionTest.java
@@ -0,0 +1,18 @@
+package model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DirectionTest {
+    @Test
+    public void EnumValues() {
+        Assertions.assertEquals("NORTH", Direction.NORTH.toString());
+        Assertions.assertEquals("EAST", Direction.EAST.toString());
+        Assertions.assertEquals("SOUTH", Direction.SOUTH.toString());
+        Assertions.assertEquals("WEST", Direction.WEST.toString());
+
+    }
+
+}
