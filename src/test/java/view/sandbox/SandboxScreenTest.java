bbefd125a0b7b5c35a8c5eab8dc40e31702848ec
CaptainZoraaaaa <97522672+CaptainZoraaaaa@users.noreply.github.com>
2023-03-06 17:30:00 +0100

    Tester för kolla ifall alla fields finns de ska ej skappas ifall dimension < 5

--- src/test/java/view/sandbox/SandboxScreenTest.java
+++ src/test/java/view/sandbox/SandboxScreenTest.java
@@ -2,6 +2,7 @@ package view.sandbox;
 
 import control.MainProgram;
 import javafx.application.Application;
+import javafx.scene.control.Label;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
@@ -21,9 +22,117 @@ class SandboxScreenTest {
     }
 
     @Test
-    void TestRightMenuLables() throws NoSuchFieldException {
+    void RightMenuPathLabel() throws NoSuchFieldException, IllegalAccessException{
         SandboxScreen sandboxScreen = new SandboxScreen(20);
         Field privateField = SandboxScreen.class.getDeclaredField("path");
+        privateField.setAccessible(true);
+        Label label = (Label) privateField.get(sandboxScreen);
+        assertTrue(label != null);
+    }
+    @Test
+    void RightMenuPathLabelInvalidDimension() throws NoSuchFieldException, IllegalAccessException{
+        SandboxScreen sandboxScreen = new SandboxScreen(-1);
+        Field privateField = SandboxScreen.class.getDeclaredField("path");
+        privateField.setAccessible(true);
+        Label label = (Label) privateField.get(sandboxScreen);
+        assertEquals(null,label);
+    }
+    @Test
+    void RightMenuWallLabel() throws NoSuchFieldException, IllegalAccessException{
+        SandboxScreen sandboxScreen = new SandboxScreen(20);
+        Field privateField = SandboxScreen.class.getDeclaredField("wall");
+        privateField.setAccessible(true);
+        Label label = (Label) privateField.get(sandboxScreen);
+        assertTrue(label != null);
+    }
+    @Test
+    void RightMenuWallLabelInvalidDimension() throws NoSuchFieldException, IllegalAccessException{
+        SandboxScreen sandboxScreen = new SandboxScreen(-1);
+        Field privateField = SandboxScreen.class.getDeclaredField("wall");
+        privateField.setAccessible(true);
+        Label label = (Label) privateField.get(sandboxScreen);
+        assertEquals(null,label);
+    }
+    @Test
+    void RightMenuHeartLabel() throws NoSuchFieldException, IllegalAccessException {
+        SandboxScreen sandboxScreen = new SandboxScreen(20);
+        Field privateField = SandboxScreen.class.getDeclaredField("heart");
+        privateField.setAccessible(true);
+        Label label = (Label) privateField.get(sandboxScreen);
+        assertTrue(label != null);
+    }
+    @Test
+    void RightMenuHeartLabelInvalidDimension() throws NoSuchFieldException, IllegalAccessException {
+        SandboxScreen sandboxScreen = new SandboxScreen(-1);
+        Field privateField = SandboxScreen.class.getDeclaredField("heart");
+        privateField.setAccessible(true);
+        Label label = (Label) privateField.get(sandboxScreen);
+        assertEquals(null,label);
+    }
 
+    @Test
+    void RightMenuBreakableWallLabel() throws NoSuchFieldException, IllegalAccessException{
+        SandboxScreen sandboxScreen = new SandboxScreen(20);
+        Field privateField = SandboxScreen.class.getDeclaredField("breakableWall");
+        privateField.setAccessible(true);
+        Label label = (Label) privateField.get(sandboxScreen);
+        assertTrue(label != null);
+    }
+    @Test
+    void RightMenuBreakableWallLabelInvalidDimension() throws NoSuchFieldException, IllegalAccessException{
+        SandboxScreen sandboxScreen = new SandboxScreen(-1);
+        Field privateField = SandboxScreen.class.getDeclaredField("breakableWall");
+        privateField.setAccessible(true);
+        Label label = (Label) privateField.get(sandboxScreen);
+        assertEquals(null,label);
+    }
+    @Test
+    void RightMenuAxeLabel() throws NoSuchFieldException, IllegalAccessException{
+        SandboxScreen sandboxScreen = new SandboxScreen(20);
+        Field privateField = SandboxScreen.class.getDeclaredField("axe");
+        privateField.setAccessible(true);
+        Label label = (Label) privateField.get(sandboxScreen);
+        assertTrue(label != null);
+    }
+    @Test
+    void RightMenuAxeLabelInvalidDimension() throws NoSuchFieldException, IllegalAccessException{
+        SandboxScreen sandboxScreen = new SandboxScreen(-1);
+        Field privateField = SandboxScreen.class.getDeclaredField("axe");
+        privateField.setAccessible(true);
+        Label label = (Label) privateField.get(sandboxScreen);
+        assertEquals(null,label);
+    }
+
+    @Test
+    void RightMenuCollectableLabel() throws NoSuchFieldException, IllegalAccessException{
+        SandboxScreen sandboxScreen = new SandboxScreen(20);
+        Field privateField = SandboxScreen.class.getDeclaredField("collectible");
+        privateField.setAccessible(true);
+        Label label = (Label) privateField.get(sandboxScreen);
+        assertTrue(label != null);
+    }
+    @Test
+    void RightMenuCollectableLabelInvalidDimension() throws NoSuchFieldException, IllegalAccessException{
+        SandboxScreen sandboxScreen = new SandboxScreen(-1);
+        Field privateField = SandboxScreen.class.getDeclaredField("collectible");
+        privateField.setAccessible(true);
+        Label label = (Label) privateField.get(sandboxScreen);
+        assertEquals(null,label);
+    }
+    @Test
+    void RightMenuDeleteLabel() throws NoSuchFieldException, IllegalAccessException{
+        SandboxScreen sandboxScreen = new SandboxScreen(20);
+        Field privateField = SandboxScreen.class.getDeclaredField("delete");
+        privateField.setAccessible(true);
+        Label label = (Label) privateField.get(sandboxScreen);
+        assertTrue(label != null);
+    }
+    @Test
+    void RightMenuDeleteLabelInvalidDimension() throws NoSuchFieldException, IllegalAccessException{
+        SandboxScreen sandboxScreen = new SandboxScreen(-1);
+        Field privateField = SandboxScreen.class.getDeclaredField("delete");
+        privateField.setAccessible(true);
+        Label label = (Label) privateField.get(sandboxScreen);
+        assertEquals(null,label);
     }
 }
