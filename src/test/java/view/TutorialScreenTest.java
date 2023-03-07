733c0b7d525fce9a7213f036ade5df797714e08b
Linus Regander <linus.regander00@gmail.com>
2023-03-07 13:16:41 +0100

    Fixat mer tester för Spacebar image
    Måste fixa mer saker dock.

--- src/test/java/view/TutorialScreenTest.java
+++ src/test/java/view/TutorialScreenTest.java
@@ -1,16 +1,38 @@
 package view;
 
+import javafx.application.Platform;
+import javafx.scene.image.Image;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class TutorialScreenTest {
+    @BeforeAll
+    static void initJFXRuntime() {
+        Platform.startup(() -> {
+        });
+    }
     @Test
     void testBackspaceIconExists() {
         File file = new File("files/skip_tutorial.png");
         assertTrue(file.exists());
+    }
+
+    @Test
+    void testBackSpaceButtonInTutorial() throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        TutorialScreen ts = new TutorialScreen();
+
+        Method skipLabel = TutorialScreen.class.getDeclaredMethod("createSkipLabel");
+        skipLabel.setAccessible(true);
+
+        Image expectedImage = new Image("file:files/skip_tutorial.png");
+        Image recieved = (Image) skipLabel.invoke(ts);
+
+        assertEquals(recieved, expectedImage);
     }
 }
