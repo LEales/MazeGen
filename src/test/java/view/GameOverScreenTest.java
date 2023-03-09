e82a62556db321bfb62750e87d312b9aaef83251
CaptainZoraaaaa <97522672+CaptainZoraaaaa@users.noreply.github.com>
2023-03-09 15:16:30 +0100

    Test för Kontroll av cause of death

--- src/test/java/view/GameOverScreenTest.java
+++ src/test/java/view/GameOverScreenTest.java
@@ -2,8 +2,16 @@ package view;
 
 import control.MainProgram;
 import javafx.application.Application;
+import javafx.scene.image.ImageView;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import view.sandbox.SandboxDimension;
+import view.sandbox.SandboxScreen;
+
+import javax.naming.directory.InvalidAttributeValueException;
+import javax.naming.directory.InvalidAttributesException;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -13,17 +21,39 @@ class GameOverScreenTest {
     static void initJFXRuntime() {
         new Thread(() -> Application.launch(MainProgram.class)).start();
     }
-/*
+
     @Test
-    void gameOverAnimation() {
+    void gameOverAnimationHeart(){
+        GameOverScreen gameOverScreen = new GameOverScreen("died");
+        ImageView imageView = (ImageView) gameOverScreen.getChildren().get(0);
+        assertEquals("file:files/texts/gameoverdied.png",imageView.getImage().getUrl());
+    }
+    @Test
+    void gameOverAnimationTime() {
+        GameOverScreen gameOverScreen = new GameOverScreen("time");
+        ImageView imageView = (ImageView) gameOverScreen.getChildren().get(0);
+        assertEquals("file:files/texts/gameovertime.png",imageView.getImage().getUrl());
+    }
+    @Test
+    void gameOverAnimationNull() {
         try {
-            Thread.sleep(2000);
-        } catch (InterruptedException ignored) {
+            GameOverScreen gameOverScreen = new GameOverScreen(null);
+            ImageView imageView = (ImageView) gameOverScreen.getChildren().get(0);
+            fail("no nullpointer check");
+        }catch (NullPointerException e){
+            assertEquals("Cause is not defined",e.getMessage());
         }
-        MainProgram mp = MainProgram.getMainProgram();
-        GameOverScreen gms = new GameOverScreen(mp);
-        assertTrue(gms.gameOverAnimation());
+    }
+    @Test
+    void gameOverAnimationWrongCause() {
+        try {
+            GameOverScreen gameOverScreen = new GameOverScreen("TJENIXEN");
+            ImageView imageView = (ImageView) gameOverScreen.getChildren().get(0);
+            fail("Not a valid cause");
+        }catch (IllegalArgumentException e){
+            assertEquals("Not a cause",e.getMessage());
+        }
     }
 
- */
+
 }
